4760db274d17a588c0791ee5560bfd492ee984ce
Hazmi <badrunsham.hazmi@gmail.com>
2026-05-21 04:38:06 +0800

    # 1

--- src/data/team.ts
+++ src/data/team.ts
@@ -10,10 +10,12 @@ export const teamMembers = [
         role: "Ketua Pegawai Eksekutif",
         image: aqilImg,
         matricNo: "2315233",
+        email: "aqil.iman@live.iium.edu.my",
+        department: "AAD, IIUM",
         description:
             "Mengurus keseluruhan perjalanan projek",
         padletUrl: "https://u1.padletusercontent.com/uploads/padlet-uploads-usc1/5551117197/2e5498002bd97ece16a09ddccf5307c5/2315233.pdf?token=HNhrAOol8WMo5BCsMyI0m4QP3W0ljyTOOA9nIOy7tBRqFMs_bwJH-bC1MFp-o13RayDP1PSghHUt2NRgRCoFyvojuNvJSSL9dMcgA5xREaDdjcjWvRR_ADXeUiOUIHjhNilbqnVQUqz13Vw0creo1aRanZkmV3GmvCu4JwTih3yzT_XsYW9kOfNd5HeCM93Qgw0OnMvkjXNAwCEM5M467G-wGkjUZamWY_JvvYD_ZchPXnhLxu032dKUGEvITETi"    
-
+        
     },
 
     {
@@ -21,6 +23,8 @@ export const teamMembers = [
         role: "Pengurus Projek",
         image: amirImg,
         matricNo: "2213389",
+        email: "amirhakim.r@live.iium.edu.my",
+        department: "BCS1, IIUM",
         description:
             "Menghasilkan reka bentuk pod",
         padletUrl: "https://u1.padletusercontent.com/uploads/padlet-uploads-usc1/5551515408/08e13d69e3a39c76591ddc7e9b0e2a96/Batu_Belah_Batu_Bertangkup.pdf?token=HNhrAOol8WMo5BCsMyI0m4QP3W0ljyTOOA9nIOy7tBRqFMs_bwJH-bC1MFp-o13RayDP1PSghHUt2NRgRCoFyvojuNvJSSL9dMcgA5xREaCRu2yFNftMvg_xb426trAK9myvaAhoCPwDwXGLxG007ATaJQvNJXRgU699KDqWLBbi0ea-LqY51QjDtPVKEu-Oi20M-HFq_YEPSCjA_jXleflwF-GkoyJQ_mMXVxcUOwoBuYyLvUxPH9h08zbxr8oSKKjU75NoI3LNZMyvI1sLmg=="
@@ -31,6 +35,8 @@ export const teamMembers = [
         role: "Pengurus Teknologi",
         image: imanImg,
         matricNo: "2318784",
+        email: "imanirdina.r@live.iium.edu.my",
+        department: "AAD, IIUM",
         description: 
             "Mengurus aspek teknologi projek",
         padletUrl: "https://u1.padletusercontent.com/uploads/padlet-uploads-usc1/5547636766/4759d7d776ff28fcae2064dc41e24fb1/Puteri_Saadong.pdf?token=HNhrAOol8WMo5BCsMyI0m4QP3W0ljyTOOA9nIOy7tBRqFMs_bwJH-bC1MFp-o13RayDP1PSghHUt2NRgRCoFyvojuNvJSSL9dMcgA5xREaB1u5nPRNo0bdfVXLwxdw8SUe6jbCE9F9QRWREloMOTW1ccqJmsDPtR8wKKVPNnxSN6V_MP3pEv0UJFWLhN-nF_WcxMYb5-QtNqIy8Fh_QDYJRHSZVyQ3dCZNLVRNrmnM3j1zpfAg8RYfOV0JbVfjun"
@@ -40,6 +46,8 @@ export const teamMembers = [
         role: "Pengurus Rekaan",
         image: najihahImg,
         matricNo: "2311514",
+        email: "amalin.azhar@live.iium.edu.my",
+        department: "AAD, IIUM",
         description:
             "Menjalankan kajian soal selidik",
         padletUrl: "https://u1.padletusercontent.com/uploads/padlet-uploads-usc1/5553012943/a98de1f76cc726a4ddb330b4b2321206/2311514.pdf?token=HNhrAOol8WMo5BCsMyI0m4QP3W0ljyTOOA9nIOy7tBRqFMs_bwJH-bC1MFp-o13RayDP1PSghHUt2NRgRCoFyvojuNvJSSL9dMcgA5xREaBP0asRnq7-r06ds755IsksmM-6o9e_ie1OVZD892qVZz-mC8UMR1w5-zJy5z8mhPSGdV4biag7z22SdP_xn97h7sUATJ5NkDMhdje1LDHIgoTVCYgqRtO8Nj3GKhkCDNFc-m6QikjBHTatmIBel65s"    
@@ -49,6 +57,8 @@ export const teamMembers = [
         role: "Pengurus Kewangan ",
         image: irrdhinaImg,
         matricNo: "2410304",
+        email: "irrdhina.amin@live.iium.edu.my",
+        department: "ALA1, IIUM",
         description:
             "Mengurus anggaran kos projek",
         padletUrl: "https://u1.padletusercontent.com/uploads/padlet-uploads-usc1/5553329656/6f9ce9b31881157aeb1bda89cb87d2fe/HUMINODUN.pdf?token=HNhrAOol8WMo5BCsMyI0m4QP3W0ljyTOOA9nIOy7tBRqFMs_bwJH-bC1MFp-o13RayDP1PSghHUt2NRgRCoFyvojuNvJSSL9dMcgA5xREaAgiL-heoobi2dHtQ-VjF9uutna2UNv_ebon_m8_CZweiLXSqu0Hq7h9czbcTtwP1XMD8yQHlHSb5fspIEYq7Z6AYJ3-IoMXLxiRC3apslnrRwQrc9Q04M9kwz7ImMEKmP7HDSXH1gtrhi-9vmAh74C"
